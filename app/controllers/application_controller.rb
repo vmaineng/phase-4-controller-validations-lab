b29159e8088bc2e845726ce86c1a1d5ae48a8072
Mai Vang <vmaineng@gmail.com>
2022-06-30 12:49:37 -0500

    commit tree 4a39222a2f27b603553d51efbf57300abb337553

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+
+
+private
+def invalid_record(exception)
+    render json: {errors: exception.record.errors}, status: :unprocessable_entity
+    #render json: {errors: "can't be blank"}, status: :unprocessable_entity
+end
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    #if need two symbols, can do 
+    #validates :name, :email, presence: true
 end
